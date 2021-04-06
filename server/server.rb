36c07ad57ac8c5a50e26597fd8d18975a3c21cba
Dain Miller <carlton.dain.miller@gmail.com>
2021-04-06 06:34:15 -0400

    Fix links to files

--- server/server.rb
+++ server/server.rb
@@ -2,12 +2,12 @@ require 'sinatra'
 require 'yaml'
 
 get '/' do
-  @db     = YAML.load(File.read('my_database.yaml'))['log']
-  @men    = File.read('health/men.txt')
-  @phys   = File.read('health/phys.txt')
-  @spir   = File.read('health/spir.txt')
-  @emo    = File.read('health/emo.txt')
-  @intel  = File.read('health/intel.txt')
-  @fin    = File.read('health/fin.txt')
+  @db     = YAML.load(File.read('../db/data/my_database.yaml'))['log']
+  @men    = File.read('../config/health/men.txt')
+  @phys   = File.read('../config/health/phys.txt')
+  @spir   = File.read('../config/health/spir.txt')
+  @emo    = File.read('../config/health/emo.txt')
+  @intel  = File.read('../config/health/intel.txt')
+  @fin    = File.read('../config/health/fin.txt')
   erb :home
 ***REMOVED***
