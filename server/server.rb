82856baa022b726f8b70bcfe000eb0e8b6a12ad3
Dain Miller <carlton.dain.miller@gmail.com>
2021-04-07 14:50:33 -0400

    Updates

--- server/server.rb
+++ server/server.rb
@@ -2,12 +2,12 @@ require 'sinatra'
 require 'yaml'
 
 get '/' do
-  @db     = YAML.load(File.read('../db/data/my_database.yaml'))['log']
-  @men    = File.read('../config/health/men.txt')
-  @phys   = File.read('../config/health/phys.txt')
-  @spir   = File.read('../config/health/spir.txt')
-  @emo    = File.read('../config/health/emo.txt')
-  @intel  = File.read('../config/health/intel.txt')
-  @fin    = File.read('../config/health/fin.txt')
+  @db     = YAML.load(File.read('db/data/my_database.yaml'))['log']
+  @men    = File.read('config/health/men.txt').to_f.round(2)
+  @phys   = File.read('config/health/phys.txt').to_f.round(2)
+  @spir   = File.read('config/health/spir.txt').to_f.round(2)
+  @emo    = File.read('config/health/emo.txt').to_f.round(2)
+  @intel  = File.read('config/health/intel.txt').to_f.round(2)
+  @fin    = File.read('config/health/fin.txt').to_f.round(2)
   erb :home
 end
